9d65ea8ab256adee941c23e6a77930ac043ae8fe
Shadi-Shwiyat <raz611103@gmail.com>
2023-08-12 14:06:57 -0700

    Added jquery to show amenities checkboxes and allow users to select them

---
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -0,0 +1,14 @@
+$(Document).ready(function () {
+    let checkemarkedAmenities = [];
+
+    $('li :checkbox').change(function () {
+        const dataName = $(this).attr("data-name");
+
+        if (this.checked) {
+            checkemarkedAmenities.push(dataName);
+        } else {
+            checkemarkedAmenities = checkemarkedAmenities.filter((item) => item !== dataName);
+        }
+        $('div.amenities h4').text(checkemarkedAmenities.join(', '));
+    });
+});
