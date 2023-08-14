a85db0ac462579400fbdf8c2631959685142cbfa
Shadi-Shwiyat <raz611103@gmail.com>
2023-08-14 14:00:18 -0700

    Fixed the css class available to properly change color of grey circle to red

--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -12,7 +12,7 @@ $(document).ready(function () {
         $('div.amenities h4').text(checkemarkedAmenities.join(', '));
     });
 
-    $.get('http://2c90ef8d54bf.654b0ff2.hbtn-cod.io:5001/api/v1/status/', function (data) {
+    $.get('http://2c90ef8d54bf.654b0ff2.hbtn-cod.io:5001/api/v1/status', function (data) {
         console.log(data.status);
         if (data.status === 'OK') {
           $('div#api_status').addClass('available');
